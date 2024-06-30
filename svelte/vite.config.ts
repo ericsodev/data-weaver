74960acb918ef9aeb0c20692fd7bea505d9022e0
Eric <eric.so.dev@gmail.com>
2024-06-29 23:09:26 -0400

    feat: start on port 3000

--- svelte/vite.config.ts
+++ svelte/vite.config.ts
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  build: {
+    sourcemap: true
+  },
+  preview: {
+    port: 3000
+  },
+  server: {
+    port: 3000
+  }
 });
