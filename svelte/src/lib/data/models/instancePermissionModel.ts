8f3b2ecef83443d82bea649797becdaba0109d9a
Eric <eric.so.dev@gmail.com>
2024-07-06 15:50:56 -0400

    feat: instance perms model

--- svelte/src/lib/data/models/instancePermissionModel.ts
+++ svelte/src/lib/data/models/instancePermissionModel.ts
@@ -1,13 +1,16 @@
-import { mixin } from 'objection';
+import { mixin, Model } from 'objection';
 import { BaseModel } from './base';
+import { Instance, type InstanceDTO } from './instanceModel';
+import { User } from './userModel';
 
-const INSTANCE_PERMISSIONS = ['ADMIN', 'WRITE', 'READ'] as const;
-type InstanceAccessTypes = (typeof INSTANCE_PERMISSIONS)[number];
+const INSTANCE_USER_ROLES = ['OWNER', 'ADMIN', 'EDITOR', 'VIEWER'] as const;
+type InstanceAccessTypes = (typeof INSTANCE_USER_ROLES)[number];
 
 export class InstancePermission extends mixin(BaseModel) {
   public instanceId!: string;
   public userId!: string;
   public accessType!: InstanceAccessTypes;
+  public instance?: InstanceDTO;
 
   static get tableName() {
     return 'instance_permission';
@@ -16,11 +19,32 @@ export class InstancePermission extends mixin(BaseModel) {
   static get jsonSchema() {
     return {
       type: 'object',
-      required: ['userId', 'instanceId', 'accessType'],
+      required: ['userId', 'instanceId', 'role'],
       properties: {
         userId: { type: 'string' },
         instanceId: { type: 'string' },
-        accessType: { enum: [...INSTANCE_PERMISSIONS] }
+        accessType: { enum: [...INSTANCE_USER_ROLES] }
+      }
+    };
+  }
+
+  static get relationMappings() {
+    return {
+      schema: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Instance,
+        join: {
+          from: 'instance_permission.instanceId',
+          to: 'instance.id'
+        }
+      },
+      user: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: User,
+        join: {
+          from: 'instance_permission.userId',
+          to: 'user.id'
+        }
       }
     };
   }
