9d5b05e270e68bd4d6de7ec382bc4b646a1502af
Eric <eric.so.dev@gmail.com>
2024-07-07 17:59:24 -0400

    feat: better db action types

--- svelte/src/lib/data/actions/baseActions.ts
+++ svelte/src/lib/data/actions/baseActions.ts
@@ -3,9 +3,22 @@ import type { BaseModel } from '../models/base';
 import type { Except, RequireAtLeastOne } from 'type-fest';
 
 type GenericDTO<T extends typeof BaseModel> = Except<InstanceType<T>, keyof Model>;
-type GenericCreateDTO<T extends typeof BaseModel> = Except<InstanceType<T>, keyof BaseModel>;
+type CreateDTO<
+  T extends typeof BaseModel,
+  E extends keyof Except<InstanceType<T>, keyof Model>
+> = Except<InstanceType<T>, keyof Model | E>;
 
-export class BaseActions<T extends typeof BaseModel> {
+type FilterDTO<
+  T extends typeof BaseModel,
+  E extends keyof Except<InstanceType<T>, keyof Model>
+> = Partial<Except<InstanceType<T>, keyof Model | E>>;
+
+// type Excluded<T extends Model, E extends keyof Except<T, keyof Model>> = Except<T, keyof Model | E>;
+
+export class BaseActions<
+  T extends typeof BaseModel,
+  E extends keyof Except<InstanceType<T>, keyof Model> = never
+> {
   protected model: T;
 
   constructor(model: T) {
@@ -34,7 +47,7 @@ export class BaseActions<T extends typeof BaseModel> {
   }
 
   async findAll(
-    filters: Partial<GenericDTO<T>>,
+    filters: FilterDTO<T, E>,
     relations?: RelationExpression<InstanceType<T>>
   ): Promise<GenericDTO<T>[] | undefined> {
     try {
@@ -53,7 +66,7 @@ export class BaseActions<T extends typeof BaseModel> {
     }
   }
 
-  async create(data: GenericCreateDTO<T>): Promise<GenericDTO<T> | undefined> {
+  async create(data: CreateDTO<T, E>): Promise<GenericDTO<T> | undefined> {
     try {
       const ret = await this.model.query().insert(data);
       return ret as unknown as GenericDTO<T>;
@@ -64,7 +77,7 @@ export class BaseActions<T extends typeof BaseModel> {
   }
 
   async update(
-    data: RequireAtLeastOne<GenericCreateDTO<T>> & { id: string }
+    data: RequireAtLeastOne<CreateDTO<T, E>> & { id: string }
   ): Promise<GenericDTO<T> | undefined> {
     try {
       const ret = await this.model.query().patchAndFetchById(data.id, data);
