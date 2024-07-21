9ac84f105ba3177aa04f91f3bee8d2aaaa8f7155
Eric <eric.so.dev@gmail.com>
2024-07-21 16:19:27 -0400

    feat: basic ui

---
+++ src/routes/instances/id/[instanceId=uuid]/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import * as Table from '$lib/components/ui/table/';
+  import * as Alert from '$lib/components/ui/alert';
+  import Button from '$lib/components/ui/button/button.svelte';
+  import AttributeRow from './AttributeRow.svelte';
+
+  let { data } = $props();
+  const onSave = () => {};
+  const error = $state();
+  let hasChanges = $state(false);
+  console.log(data.instance);
+</script>
+
+<header class="mb-8 flex justify-between">
+  <span>
+    <p class="text-xl text-muted-foreground">Instance</p>
+    <h2 class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+      {data.instance.name}
+    </h2>
+  </span>
+  <!-- {#if canDelete} -->
+  <!--   <Button onclick={handleDelete} size="sm" variant="outline" class="self-end" -->
+  <!--     ><Trash2 class="w-4 mr-2"></Trash2> Delete</Button -->
+  <!--   > -->
+  <!-- {/if} -->
+</header>
+<div>
+  <Table.Root class="w-full">
+    <Table.Header>
+      <Table.Row>
+        <Table.Head class="min-w-[180px] w-60">Name</Table.Head>
+        <Table.Head class="min-w-[120px] w-60">Type</Table.Head>
+        <Table.Head class="w-96">Value</Table.Head>
+        <Table.Head class="w-20">Reset</Table.Head>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+      {#each data.instance.schema?.attributes ?? [] as attribute}
+        <AttributeRow {attribute}></AttributeRow>
+      {/each}
+    </Table.Body>
+  </Table.Root>
+  {#if data.abilities.includes('ATTRIBUTE:WRITE') && hasChanges}
+    <Button on:click={onSave} size="lg" class="mt-6">Save changes</Button>
+  {/if}
+  {#if error}
+    <Alert.Root class="w-60 bg-red-400/30 border-red-500 border-[1.5px] fixed top-10 right-10">
+      <Alert.Title>Error saving</Alert.Title>
+      <Alert.Description>{error}</Alert.Description>
+    </Alert.Root>
+  {/if}
+</div>
---
+++ src/routes/instances/id/[instanceId=uuid]/AttributeRow.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import Button from '$lib/components/ui/button/button.svelte';
+  import { Checkbox } from '$lib/components/ui/checkbox';
+  import Input from '$lib/components/ui/input/input.svelte';
+  import { TableCell, TableRow } from '$lib/components/ui/table';
+  import type { AttributeDTO } from '$lib/data/models/attributeModel';
+  import { AsteriskIcon, RotateCcwIcon } from 'lucide-svelte';
+
+  interface IProps {
+    attribute: AttributeDTO;
+  }
+
+  let { attribute }: IProps = $props();
+</script>
+
+<TableRow>
+  <TableCell>{attribute.name}</TableCell>
+  <TableCell class="text-muted-foreground">
+    {attribute.type}
+    {#if attribute.required}
+      <AsteriskIcon class="ml-0.5 w-3.5 inline-block text-red-400"></AsteriskIcon>
+    {/if}
+  </TableCell>
+  <TableCell>
+    {#if attribute.type === 'string'}
+      <Input type="text" required={attribute.required}></Input>
+    {:else if attribute.type === 'number'}
+      <Input type="number" required={attribute.required}></Input>
+    {:else}
+      <Checkbox></Checkbox>
+    {/if}
+  </TableCell>
+
+  <TableCell
+    ><Button variant="outline" size="icon">
+      <RotateCcwIcon class="w-4"></RotateCcwIcon>
+    </Button>
+  </TableCell>
+</TableRow>
