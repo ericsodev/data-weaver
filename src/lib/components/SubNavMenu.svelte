e133ff16865b81a43a06363eb9e0c70ac439794b
Eric <eric.so.dev@gmail.com>
2024-08-04 00:42:12 -0400

    feat: collapse side bar

--- src/lib/components/SubNavMenu.svelte
+++ src/lib/components/SubNavMenu.svelte
@@ -5,12 +5,19 @@
 <script lang="ts">
   import { page } from '$app/stores';
   import Button from '$lib/components/ui/button/button.svelte';
-  import { Icon } from 'lucide-svelte';
+  import { cn } from '$lib/utils';
+  import { ChevronDown, ChevronUp, Icon, Menu } from 'lucide-svelte';
   import type { ComponentType } from 'svelte';
-  export let routes: Route[];
-  export let rootUri: string;
+  import { fade } from 'svelte/transition';
 
-  $: rootUri = rootUri.endsWith('/') ? rootUri : rootUri + '/';
+  interface Props {
+    routes: Route[];
+    rootUri: string;
+  }
+
+  let { routes, rootUri }: Props = $props();
+
+  rootUri = rootUri.endsWith('/') ? rootUri : rootUri + '/';
   // Checks if rootUri + uri is a prefix of the current URI
   function checkIsPrefix(uri: string): boolean {
     if (uri === '#separator' || uri === '#label') {
@@ -20,9 +27,10 @@
     return $page.url.pathname.startsWith(rootUri + uri);
   }
 
-  let activeUrl: string;
   const update = (_p) => routes.filter((r) => checkIsPrefix(r.id))[0] ?? routes[0];
-  $: activeUrl = update($page)?.id ?? '';
+  let activeUrl = $derived(update($page)?.id ?? '');
+
+  let openMenu = $state(false);
 </script>
 
 <div class="md:flex h-full w-full flex-col items-stretch gap-2.5 hidden">
@@ -47,25 +55,46 @@
     {/if}
   {/each}
 </div>
-<div class="flex h-full w-full flex-col items-stretch gap-2.5 md:hidden">
-  {#each routes as { id, name, icon }}
-    {#if id === '#separator'}
-      <hr class="mb-2" />
-    {:else if id === '#label'}
-      <p class="ml-3 text-sm text-muted-foreground">{name}</p>
-    {:else}
-      <Button
-        href={rootUri + id}
-        variant={activeUrl === id ? 'secondary' : 'ghost'}
-        class="justify-start text-start"
-      >
-        {#if icon}
-          <svelte:component this={icon} class="w-5 mr-3"></svelte:component>
+<div class=" md:hidden">
+  <Button
+    variant="link"
+    class={cn('text-muted-foreground mb-2', openMenu && 'text-foreground')}
+    on:click={() => (openMenu = !openMenu)}
+  >
+    <span class="mr-2">
+      {#if openMenu}
+        <ChevronDown class="w-4"></ChevronDown>
+      {:else}
+        <ChevronUp class="w-4"></ChevronUp>
+      {/if}
+    </span>
+    Menu</Button
+  >
+  {#if openMenu}
+    <div
+      transition:fade={{ delay: 10, duration: 100 }}
+      class="flex h-full w-full flex-col items-stretch gap-2.5 rounded-md border-muted-foreground border-[1px] pb-5"
+    >
+      {#each routes as { id, name, icon }}
+        {#if id === '#separator'}
+          <hr class="mb-2" />
+        {:else if id === '#label'}
+          <p class="ml-3 text-sm text-muted-foreground">{name}</p>
         {:else}
-          <span class="w-8"></span>
+          <Button
+            href={rootUri + id}
+            variant={activeUrl === id ? 'secondary' : 'ghost'}
+            class="justify-start text-start"
+          >
+            {#if icon}
+              <svelte:component this={icon} class="w-5 mr-3"></svelte:component>
+            {:else}
+              <span class="w-8"></span>
+            {/if}
+            {name}</Button
+          >
         {/if}
-        {name}</Button
-      >
-    {/if}
-  {/each}
+      {/each}
+    </div>
+  {/if}
 </div>
