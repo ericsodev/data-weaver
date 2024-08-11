94ff1a635887b7c4a5601475e89c23ced647b52b
Eric <eric.so.dev@gmail.com>
2024-08-11 13:45:57 -0400

    feat: delete users

--- src/routes/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import { removePrototype } from '$lib/utils/toPojo';
-import type { LayoutServerLoad } from './$types';
-
-export const load: LayoutServerLoad = async ({ locals }) => {
-  return {
-    user: locals.user && removePrototype(locals.user)
-  };
-};
