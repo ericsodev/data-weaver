7597840b2e5092462a4ce88dff06aba3e5a00a90
Eric <eric.so.dev@gmail.com>
2024-09-08 00:00:04 -0400

    fix: redacted user type

--- src/routes/authed/dashboard/roles/types.ts
+++ src/routes/authed/dashboard/roles/types.ts
@@ -1,6 +1,6 @@
 import type { USER_ROLES } from '$lib/data/models/role.model.js';
-import type { UserDTO } from '$lib/data/models/user.model.js';
+import type { RedactedUserDTO } from '$lib/data/models/user.model.js';
 import type { OverrideProperties } from 'type-fest';
 
 export type SelectedRoles = Partial<Record<USER_ROLES, boolean>>;
-export type User = OverrideProperties<UserDTO, { roles: SelectedRoles }>;
+export type User = OverrideProperties<RedactedUserDTO, { roles: SelectedRoles }>;
